c86881435cb870811cc08d2f04933f56c3cfb184
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2021-11-09 05:39:50 +0900

    第20章までできた

--- xunit.py
+++ xunit.py
@@ -3,26 +3,28 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
-class TestCaseTest(TestCase):
-    def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
-        
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
+    def setUp(self):
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
-
-TestCaseTest("testRunning").run()    
-#test = WasRun("testMethod")
-#print(test.wasRun)
-#test.run()
-#print(test.wasRun)
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+        
+class TestCaseTest(TestCase):
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+        
+TestCaseTest("testTemplateMethod").run()
